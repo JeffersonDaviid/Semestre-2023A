a82adc1bba7f084c5ab3d51535fd06ae40f8b790
Jefferson David <jeffersonchile2001@gmail.com>
2023-06-15 19:33:33 -0500

    algoritmo lista, crear inicio-final, buscar y recorrer

--- EDA1/src/Unit7Listas/ListaSimple.java
+++ EDA1/src/Unit7Listas/ListaSimple.java
@@ -3,31 +3,26 @@ package Unit7Listas;
 import javax.swing.JOptionPane;
 
 public class ListaSimple {
-    private static String lista;
-    private Nodo primero;
-
-    public ListaSimple() {
-        this.primero = null;
-    }
+    private static String lista = "";
 
     public static void mostrarLista(Nodo primerNodo) {
         Nodo nodoAux = primerNodo;
         while (nodoAux.getSiguiente() != null) {
             lista += nodoAux.getDato() + "-> ";
-            nodoAux.setSiguiente(nodoAux.getSiguiente());
+            nodoAux = nodoAux.getSiguiente();
         }
         lista += nodoAux.getDato() + "-> NULL";
         JOptionPane.showMessageDialog(null, lista);
-
+        lista = "";
     }
 
     public static void crearNodoAlInicio() {
-
-        Nodo primero = new Nodo(" primer dato ");
+        String info = JOptionPane.showInputDialog(null, "Ingrese la info del nodo");
+        Nodo primero = new Nodo(info);
 
         int option = Integer.parseInt(JOptionPane.showInputDialog(null, "Desea escribir más nodos: SI=1 o NO=0"));
         while (option == 1) {
-            String info = JOptionPane.showInputDialog(null, "Ingrese la info del nodo");
+            info = JOptionPane.showInputDialog(null, "Ingrese la info del nodo");
             Nodo Q = new Nodo(info, primero);
             primero = Q;
             option = Integer.parseInt(JOptionPane.showInputDialog(null, "Desea escribir más nodos: SI=1 o NO=0"));
@@ -37,7 +32,41 @@ public class ListaSimple {
 
     }
 
+    public static void crearNodoAlFinal() {
+        String info = JOptionPane.showInputDialog(null, "Ingrese la info del nodo");
+        Nodo primero = new Nodo(info);
+        int option = Integer.parseInt(JOptionPane.showInputDialog(null, "Desea escribir más nodos: SI=1 o NO=0"));
+        while (option == 1) {
+            Nodo nodoAux = primero;
+            while (nodoAux.getSiguiente() != null) {
+                nodoAux = nodoAux.getSiguiente();
+            }
+            info = JOptionPane.showInputDialog(null, "Ingrese la info del nodo");
+            Nodo nuevoNodo = new Nodo(info);
+            nodoAux.setSiguiente(nuevoNodo);
+            option = Integer.parseInt(JOptionPane.showInputDialog(null, "Desea escribir más nodos: SI=1 o NO=0"));
+        }
+        mostrarLista(primero);
+        info = JOptionPane.showInputDialog(null, "Ingrese el dato que desea buscar en la lista");
+        buscarNodoEnLista(primero, info);
+    }
+
+    public static void buscarNodoEnLista(Nodo primerNodo, String datoSolicitado) {
+        Nodo nodoAux = primerNodo;
+        boolean isFind = false;
+        int position = 0;
+        while (nodoAux.getSiguiente() != null && isFind == false) {
+            position++;
+            if (datoSolicitado.equals(nodoAux.getDato()))
+                isFind = true;
+
+            nodoAux = nodoAux.getSiguiente();
+        }
+        JOptionPane.showMessageDialog(null, "El dato '" + datoSolicitado + "' se encuentra en el nodo #" + position);
+    }
+
     public static void main(String[] args) {
-        crearNodoAlInicio();
+        // crearNodoAlInicio();
+        crearNodoAlFinal();
     }
 }
